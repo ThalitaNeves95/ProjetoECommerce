3d3c80f58880650f83c28d2a687121e20fb174af
ThalitaNeves95 <thalita.neves24@gmail.com>
2025-04-30 19:52:35 -0300

    habilitar autorizacao no swagger

--- API-ECommerce/API-ECommerce/Program.cs
+++ API-ECommerce/API-ECommerce/Program.cs
@@ -4,13 +4,40 @@ using API_ECommerce.Context;
 using API_ECommerce.Interfaces;
 using API_ECommerce.Repositories;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 // 1 - NUNCA MEXER - Onde começa meu projeto/Program.cs - Sempre a primeira linha
 var builder = WebApplication.CreateBuilder(args);
 // 3- 
 builder.Services.AddControllers();
 // 4 -
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "JWTToken_Auth_API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Cabeçalho de autorização JWT usando o esquema Bearer. \r\n\r\n Insira 'Bearer' [espaço] e, em seguida, seu token na entrada de texto abaixo.\r\n\r\nExemplo: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 // O .NET vai criar os objetos (Injeção de Dependencia)
 // AddTransiente - O C# cria uma instancia nova, toda vez que um método é chamado
--- API-ECommerce/API-ECommerce/obj/Debug/net9.0/API-ECommerce.AssemblyInfo.cs
+++ API-ECommerce/API-ECommerce/obj/Debug/net9.0/API-ECommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c28c5a3dde26dfd84ce83407091d6aa3a857bd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6d9f3835818b2d2cd9863ca921a88d5f421cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
